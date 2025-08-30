d5b63ae54ef3797e4f6fdfcc095ffee4d8fa71b7
baharrizqi1381-pixel <baharrizqi1381@gmail.com>
2025-08-30 16:42:35 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aman App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Aman App")),
+      body: const Center(child: Text("Hello, Flutter with embedding v2!")),
+    );
+  }
+}
